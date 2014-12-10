5119d2613beeba45becf5300013d0f3f2b4b556f
Tomasz Polanski <tomasz.polanski@futurice.com>
2014-12-10 13:22:48 +0100

    Creating Luncher console application

---
+++ LuncherConsole/Luncher.fs
@@ -0,0 +1,34 @@
+﻿module LuncherConsole.Luncher
+
+module private PrivateLuncher = 
+    open FSharp.Data
+    
+    let rnd = System.Random()
+    
+    let rec randomizeRestaurants (l : string list) = 
+        seq { 
+            if l.Length <> 0 then 
+                let number = l.[rnd.Next(l.Length)]
+                yield number
+                yield! randomizeRestaurants (l |> List.filter ((<>) number))
+        }
+    
+    let rec getNotVisitedRestaruants allRestaurants visited = 
+        match allRestaurants with
+        | [] -> []
+        | x :: xs -> 
+            if visited |> List.exists ((=) x) then getNotVisitedRestaruants xs visited
+            else x :: getNotVisitedRestaruants xs visited
+    
+    type data = CsvProvider< "Restaurants.csv", ";" >
+    
+    let restaurantData = data.Load("Restaurants.csv")
+    
+    let Restaurants() : string list = 
+        restaurantData.Rows
+        |> Seq.map (fun r -> r.Name)
+        |> Seq.toList
+        |> randomizeRestaurants
+        |> Seq.toList
+
+let GetNotVisitedRestaurants visited = PrivateLuncher.getNotVisitedRestaruants (PrivateLuncher.Restaurants()) visited
---
+++ LuncherConsole/Program.fs
@@ -0,0 +1,16 @@
+﻿let imHungry : Unit = 
+    let rec imHungryRec visited = 
+        match LuncherConsole.Luncher.GetNotVisitedRestaurants visited with
+        | [] -> 
+            printfn "Nothing left, you are sooo picky!"
+            printf "Try again."
+            System.Console.ReadLine() |> ignore
+            imHungryRec []
+        | x :: xs -> 
+            printf "How about %A?" x
+            System.Console.ReadLine() |> ignore
+            imHungryRec (x :: visited)
+    imHungryRec []
+
+[<EntryPoint>]
+let main argv = imHungry; 0 // return an integer exit code
