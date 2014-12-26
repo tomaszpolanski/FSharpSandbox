a10df2bd2abe9ca47cdac0951e5aa8c54618c5b3
tomaszpolanski <krigerek@gmail.com>
2014-12-26 17:44:31 +0100

    Implementing view model logic

--- Luncher.ViewModels/MainPageViewModel.cs
+++ Luncher.ViewModels/MainPageViewModel.cs
@@ -1,18 +1,48 @@
 ﻿using Luncher.Api;
 using System.Collections.Generic;
 using System.Linq;
+using Utilities.Reactive;
+using System;
+using System.Reactive.Linq;
 
 namespace Luncher.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
-        private readonly IEnumerable<RestaurantType> _notVisitedRestaurants;
 
-        public string Textt { get; set; }
+        public ReadonlyReactiveProperty<string> RestaurantText { get; private set; }
+
+        public ReactiveCommand NextCommand { get; private set; }
+
         public MainPageViewModel()
         {
-            _notVisitedRestaurants = LuncherApi.ImHungry(LuncherApi.GetRestaurants("1,2,3,4,5,6"));
-            Textt = _notVisitedRestaurants.FirstOrDefault().name;
+            NextCommand = new ReactiveCommand();
+            var restaurantObservable = DefineRestaurants("1,2,3,4,5,6");
+            RestaurantText = DefineRestaurantText(NextCommand, restaurantObservable)
+                       .ToReadonlyReactiveProperty(string.Empty);
         }
+
+        private static IObservable<RestaurantType> DefineRestaurants(string restaurantList)
+        {
+            return LuncherApi.ImHungry(LuncherApi.GetRestaurants(restaurantList)).Take(100)
+                .ToObservable()
+                .Publish()
+                .RefCount();
+        }
+
+        private static IObservable<string> DefineRestaurantText(IObservable<object> commandTrigger, IObservable<RestaurantType> restaurantObservable)
+        {
+            return commandTrigger.StartWith((object)null)
+                       .Zip(restaurantObservable, (_, restaurant) => restaurant)
+                       .Select(restaurant => restaurant.name)
+                       .Select(TextDescription);
+        }
+
+        private static string TextDescription(string restaurant)
+        {
+            return string.IsNullOrEmpty(restaurant) ? "You are sooo picky!\nTry again!"
+                                                    : string.Format("How about {0}?", restaurant);
+        }
+
     }
 }
